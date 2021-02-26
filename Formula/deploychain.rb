2d6b12f2eea09cc86bd2cf0295e2f1d303f08646
Marian Andre <marian.andre@icloud.com>
2021-02-26 23:56:12 +0100

    Brew formula update for deploychain version v0.0.2

--- Formula/deploychain.rb
+++ Formula/deploychain.rb
@@ -11,11 +11,11 @@ class Deploychain < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://s3.andre.sk/deploychain/deploychain/0.0.2/deploychain_0.0.2_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "4dd4bbb5df8012cae4e249f2026f6f2a9b871a306c7276a22ae0b5f03ebc02bb"
+    sha256 "d167050ad30f6692cba07d70ba911a048cf0ed3882191401d488c38c3282aaa5"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://s3.andre.sk/deploychain/deploychain/0.0.2/deploychain_0.0.2_darwin_arm64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "61b0b80ed3f09baf4dd353fbc1289b1afcecd74154b388af733624a79979289d"
+    sha256 "c8f0ef2ebe8421c47373544f7c836def8899ec07600cb91495c0592b6fd80004"
   end
 
   def install
